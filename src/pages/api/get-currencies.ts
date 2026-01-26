d12f28a578c7961a667fd5fdd354782e78d25377
Narixius <nariman.movaffaghi@gmail.com>
2026-01-27 01:11:30 +0330

    refactor: replace API route for fetching currencies with direct import

--- src/pages/api/get-currencies.ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from "astro";
-
-import { getCurrencies } from "@/lib/now-payment";
-
-export const prerender = false;
-export const GET: APIRoute = async () => {
-  return getCurrencies();
-};
